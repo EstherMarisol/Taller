21534e89cfd3d47ae5f0c7c1f5888cdfaae99356
Esther <esthermarigomez18@gmail.com>
2020-05-21 09:34:44 -0400

    Ordenamos las llaves, segun las convenciones de java

--- Quicksort/src/Ordenamiento.java
+++ Quicksort/src/Ordenamiento.java
@@ -1,11 +1,9 @@
 import java.util.*;
-public class Ordenamiento 
-{
+public class Ordenamiento {
 	int []vec; int n;
 	Scanner leer = new Scanner(System.in);
 	
-	Ordenamiento()
-	{
+	Ordenamiento(){
 		System.out.println("ingrese el tama�o del vector");
 		n=leer.nextInt();
 		vec=new int[n];
@@ -14,38 +12,33 @@ public class Ordenamiento
 		mostrar(vec);
 	}
 	
-	public void llenar()
-	{
+	public void llenar(){
 		System.out.println("ingrese los datos del vector");
 		for (int i=0;i<n;i++)
 		{
 			vec[i]=leer.nextInt();
 		}
 	}
-	public void mostrar(int vec[])
-	{
+	public void mostrar(int vec[]){
 		System.out.println("el vector ordenado es: ");
 		for (int i=0;i<vec.length;i++)
 		{
 			System.out.print(vec[i]+" ");
 		}
 	}
-	 public int[] quicksort1(int numeros[])
-	 {
+	 public int[] quicksort1(int numeros[]){
 	   return quicksort2(numeros,0,numeros.length-1); 
 	 }
-	 public int[] quicksort2(int numeros[],int izq, int der)
-	 {
+
+	 public int[] quicksort2(int numeros[],int izq, int der) {
 	   if(izq>=der)
 		   return numeros;
 	   int i=izq, d=der;
-	   if(izq!=der)
-	   {
+	   if(izq!=der){
 		    int pivote;
 		    int aux;
 		    pivote = izq;
-		    while(izq!=der)
-		    {
+		    while(izq!=der){
 			    while(numeros[der]>=numeros[pivote] && izq<der)
 			    	 der--;
 			    while(numeros[izq]<numeros[pivote] && izq<der)
@@ -63,9 +56,8 @@ public class Ordenamiento
 			    quicksort2(numeros,izq+1,d);
 		   }
 		   mostrar(numeros);
-	   }
-	   else
-	   {
+}
+	   else{
 		   mostrar(numeros);
 		   return numeros;
 	   }  
